a621ce51d855fcb967fa4ef1c7ad7f10224b384c
Tiago Justino <tjustino@gryd.com>
2026-03-14 11:32:45 -0400

    feat: initialize frontend workspace with `npm create vite@latest` (react/typescript)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Pendulum Simulation</h1>
+          <p>
+           This is a Simple Pendulum simulation using React and Typescript.
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
