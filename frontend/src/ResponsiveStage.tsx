deafa23ffd401b2d0b0b73bc0d9a36304772011a
Tiago Justino <tjustino@gryd.com>
2026-03-14 12:20:54 -0400

    feat: draws a stub pendulum

--- frontend/src/ResponsiveStage.tsx
+++ frontend/src/ResponsiveStage.tsx
@@ -1,14 +1,16 @@
 import { useState, useEffect, useRef } from "react";
-import { Stage, Layer, Circle } from "react-konva";
+import { Stage, Layer, Circle, Line } from "react-konva";
+import type { Point } from "@pendulum-simulation/common";
 
 const ResponsiveStage = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const [pivotPosition, setPivotPosition] = useState<Point>({ x: 0, y: 0 });
+  const [bobPosition, setBobPosition] = useState<Point>({ x: 0, y: 0 });
 
   useEffect(() => {
     const updateDimensions = () => {
       if (containerRef.current) {
-        console.log("New container dimensions:", containerRef.current.offsetWidth, containerRef.current.offsetHeight);
         setDimensions({
           width: containerRef.current.offsetWidth,
           height: containerRef.current.offsetHeight,
@@ -25,17 +27,52 @@ const ResponsiveStage = () => {
     };
   }, []);
 
+  useEffect(() => {
+    setPivotPosition({
+      x: dimensions.width / 2,
+      y: 25,
+    });
+  }, [dimensions]);
+
+  useEffect(() => {
+    setBobPosition({
+      x: pivotPosition.x + 100,
+      y: pivotPosition.y + 300,
+    });
+  }, [pivotPosition]);
+
   return (
     <div
       ref={containerRef}
-      style={{ width: "100%", flex: 1, overflow: "hidden", border: "1px solid grey" }}
+      style={{
+        width: "100%",
+        flex: 1,
+        overflow: "hidden",
+        border: "1px solid grey",
+      }}
     >
       <Stage width={dimensions.width} height={dimensions.height}>
         <Layer>
           <Circle
-            x={dimensions.width / 2}
-            y={dimensions.height / 2}
-            radius={50}
+            x={pivotPosition.x}
+            y={pivotPosition.y}
+            radius={5}
+            fill="black"
+          />
+          <Line
+            points={[
+              pivotPosition.x,
+              pivotPosition.y,
+              bobPosition.x,
+              bobPosition.y,
+            ]}
+            strokeWidth={2}
+            stroke="black"
+          />
+          <Circle
+            x={bobPosition.x}
+            y={bobPosition.y}
+            radius={25}
             fill="black"
           />
         </Layer>
